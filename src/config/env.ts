1be0c392aff2d9c7542e46266b43b5cf0f648a0e
Vitório Mota <154383083+vitorio81@users.noreply.github.com>
2025-06-17 08:47:24 -0300

    feat: adicionando lista de servidores

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,9 +5,6 @@ export const config = {
   port: process.env.PORT,
   secretKayHomeAssistant: process.env.SECRET_KAY_HOME_ASSITANT,
   host : process.env.HOST,
-  zabbixServer: process.env.ZABBIX_SERVER,
-  zabbixHost: process.env.ZABBIX_HOST,
-  zabbixUser: process.env.ZABBIX_USER,
-  zabbixPassword: process.env.ZABBIX_PASSWORD,
+  zabbixServer: process.env.ZABBIX_SERVER || "[]",
   zabbixHostSensores: process.env.ZABBIX_HOST_SENSORES,
 };
--- src/services/ToSendZabbix.ts
+++ src/services/ToSendZabbix.ts
@@ -6,17 +6,21 @@ export class ToSendZabbix {
     sensorId: string,
     temperature: string
   ): Promise<void> {
-    const cmd = `zabbix_sender -z ${config.zabbixServer} -s "${config.zabbixHostSensores}" -k ${sensorId} -o ${temperature}`;
-    exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Erro ao enviar para o Zabbix: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-    });
+    const servers = (config.zabbixServer || "").split(",").map((s) => s.trim());
+
+    for (const server of servers) {
+      const cmd = `zabbix_sender -z ${server} -s "${config.zabbixHostSensores}" -k ${sensorId} -o ${temperature}`;
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Erro ao enviar para o Zabbix: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
+    }
   }
 }
